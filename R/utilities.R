6068402622a3509501fc758f05e18fa97a42b5f8
jprice80 <39910570+jprice80@users.noreply.github.com>
2020-11-16 13:02:35 -0500

    Created a function to identify the best set of rules to apply to each term

--- R/utilities.R
+++ R/utilities.R
@@ -13,6 +13,7 @@ dataClasses <- function(model){
   return(out)
 }
 
+
 #====================================== Model Properties ===================================================
 
 
@@ -57,25 +58,6 @@ model_properties <- function(model){
     }
   }
 
-  # Determine if we have a random int, slope, or int_and_slope
-  prop_out$termtype <- as.character(NA)
-  for(i in 1:nrow(prop_out)) {
-
-    slope_term <- prop_out$slopeterm[i]
-    containsneg1 <- grepl("- 1", slope_term, fixed = TRUE)
-    containspls0 <- grepl("+ 0", slope_term, fixed = TRUE)
-
-    if(!is.na(slope_term) == TRUE) {
-      if(slope_term == "1") {
-        prop_out$termtype[i] <- "int"
-      } else if (containsneg1 == TRUE || containspls0 == TRUE) {
-        prop_out$termtype[i] <- "slope"
-      } else {
-        prop_out$termtype[i] <- "int_and_slope"
-      }
-    }
-  }
-
   # Define fixed and random terms
   prop_out$vartype <- as.character(NA)
   for(i in 1:nrow(prop_out)) {
@@ -103,5 +85,109 @@ model_properties <- function(model){
     }
   }
 
+  # Determine if we have a random int, slope, or int_and_slope
+  prop_out$termtype <- as.character(NA)
+  for(i in 1:nrow(prop_out)) {
+
+    slope_term <- prop_out$slopeterm[i]
+
+    # Identify the slope class
+    if(slope_term != "1" && is.na(slope_term) == FALSE){
+
+      containsneg1 <- grepl("- 1", slope_term, fixed = TRUE)
+      containspls0 <- grepl("+ 0", slope_term, fixed = TRUE)
+
+      if(containsneg1 == TRUE) {
+        eval_slope <- trimws(strsplit(as.character(slope_term), "\\- ")[[1]][1])
+      } else if(containspls0 == TRUE) {
+        eval_slope <- trimws(strsplit(as.character(slope_term), "\\+ ")[[1]][1])
+      } else {
+        rslope_int <- TRUE
+      }
+
+      if(containsneg1 == TRUE || containspls0 == TRUE || rslope_int == TRUE) {
+        fixed <- prop_out[prop_out$vartype=="fixed", ]
+        slope_class <- fixed[which(fixed$terms == eval_slope), "varclass"]
+      } else {
+        slope_class <- "factor"
+      }
+    }
+
+
+    if(!is.na(slope_term) == TRUE) {
+      if(slope_term == "1" || slope_class == "factor") {
+        prop_out$termtype[i] <- "int"
+      } else if (containsneg1 == TRUE || containspls0 == TRUE) {
+        prop_out$termtype[i] <- "slope"
+      } else {
+        prop_out$termtype[i] <- "int_and_slope"
+      }
+    }
+  }
+
   return(prop_out)
 }
+
+
+#====================================== Best Rules ===================================================
+
+
+#' Identify the Most Appropriate Rules to Apply to each Fixed Effect Term
+#'
+#' @param model
+#'
+#' @return a \code{\link{data.frame}}
+#' @keywords internal
+best_rules <- function(model){
+  mp<-model_properties(model)
+
+  fixed <- mp[mp$vartype == "fixed",]
+  random <- mp[mp$vartype == "random",]
+
+  # replace p0 n1 terms
+  for(i in 1:nrow(random)) {
+
+    termtype <- random$termtype[i]
+
+    if(termtype == "slope"){
+
+      slope_term <- random$slopeterm[i]
+
+      containsneg1 <- grepl("- 1", slope_term, fixed = TRUE)
+      containspls0 <- grepl("+ 0", slope_term, fixed = TRUE)
+
+      if(containsneg1 == TRUE) {
+        random$slopeterm[i] <- trimws(strsplit(as.character(slope_term), "\\- ")[[1]][1])
+      } else if(containspls0 == TRUE) {
+        random$slopeterm[i] <- trimws(strsplit(as.character(slope_term), "\\+ ")[[1]][1])
+      }
+    }
+  }
+
+  # Determine the best rules to apply to each fixed effect
+  slope_term_vec <- random$slopeterm
+  rules_out <- data.frame()
+  for(i in 1:nrow(fixed)) {
+
+    term <- fixed$terms[i]
+    pos <-which(slope_term_vec %in% term)
+
+    if(identical(pos, integer(0))){
+      rules <- "int"
+      slope_term <- "1"
+      intercept_term <- as.character(NA)
+      covar <- as.character(NA)
+    } else {
+      rules <- random$termtype[pos]
+      slope_term <- random$slopeterm[pos]
+      intercept_term <- random$terms[pos]
+      covar <- random$covar[pos]
+    }
+
+    currow <- data.frame(terms = term, slopeterm=slope_term, interceptterm=intercept_term, covar, rules)
+    rules_out <- rbind(rules_out, currow)
+  }
+
+  return(rules_out)
+}
+
